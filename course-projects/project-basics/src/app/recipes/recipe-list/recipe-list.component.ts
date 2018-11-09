5fde1ae6d5acf7858da7d77eaccc7e609731c1de
andrejmalykhin <Andrej.Malykhin.13@gmail.com>
2018-11-09 15:21:17 +0200

    2. Course project - the basics

---
+++ course-projects/project-basics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://get.pxhere.com/photo/dish-meal-food-recipe-breakfast-cuisine-vegetarian-food-italian-food-turkish-food-marinara-sauce-european-food-middle-eastern-food-menemen-1417896.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://get.pxhere.com/photo/dish-meal-food-recipe-breakfast-cuisine-vegetarian-food-italian-food-turkish-food-marinara-sauce-european-food-middle-eastern-food-menemen-1417896.jpg')
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
